2d973fcb6a6e2f5c10c38a8a098f673c4358760e
Akramul Ratul <68425589+akramulratul@users.noreply.github.com>
2023-02-05 03:32:45 +0600

    Update hoverAnimation.js

--- hoverAnimation.js
+++ hoverAnimation.js
@@ -1,9 +1,9 @@
    "use strict";(() => {
-      $.easing.easeInOutQuad = function (x, t, b, c, d) {
-        if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
-        return (-c / 2) * (--t * (t - 2) - 1) + b;
-      };
-      $(document).ready(function () {
+       $(document).ready(function () {
+        $.easing.easeInOutQuad = function (x, t, b, c, d) {
+          if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
+          return (-c / 2) * (--t * (t - 2) - 1) + b;
+        };
         $("#1h").hover(
           function () {
             $("#2h").animate(
@@ -64,8 +64,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
         $("#2h").hover(
           function () {
             $("#1h").animate(
@@ -126,8 +124,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
         $("#3h").hover(
           function () {
             $("#1h").animate(
@@ -188,8 +184,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
         $("#4h").hover(
           function () {
             $("#1h").animate(
@@ -250,10 +244,7 @@
               );
           }
         );
-      });
 
-      //footer animate function
-      $(document).ready(function () {
         $("#1f").hover(
           function () {
             $("#2f").animate(
@@ -386,8 +377,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
         $("#2f").hover(
           function () {
             $("#1f").animate(
@@ -520,142 +509,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
-        $("#2f").hover(
-          function () {
-            $("#1f").animate(
-              {
-                color: $.Color("#3c474f"),
-              },
-              {
-                duration: 200,
-                easing: "easeInOutQuad",
-              }
-            ),
-              $("#3f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#4f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#5f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#6f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#7f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#8f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              );
-          },
-          function () {
-            $("#1f").animate(
-              {
-                color: $.Color("#ffffff"),
-              },
-              {
-                duration: 200,
-                easing: "easeInOutQuad",
-              }
-            ),
-              $("#3f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#4f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#5f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#6f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#7f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#8f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              );
-          }
-        );
-      });
-      $(document).ready(function () {
         $("#3f").hover(
           function () {
             $("#1f").animate(
@@ -788,8 +641,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
         $("#4f").hover(
           function () {
             $("#1f").animate(
@@ -922,8 +773,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
         $("#5f").hover(
           function () {
             $("#1f").animate(
@@ -1056,8 +905,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
         $("#6f").hover(
           function () {
             $("#1f").animate(
@@ -1190,142 +1037,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
-        $("#6f").hover(
-          function () {
-            $("#1f").animate(
-              {
-                color: $.Color("#3c474f"),
-              },
-              {
-                duration: 200,
-                easing: "easeInOutQuad",
-              }
-            ),
-              $("#2f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#3f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#4f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#5f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#7f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#8f").animate(
-                {
-                  color: $.Color("#3c474f"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              );
-          },
-          function () {
-            $("#1f").animate(
-              {
-                color: $.Color("#ffffff"),
-              },
-              {
-                duration: 200,
-                easing: "easeInOutQuad",
-              }
-            ),
-              $("#2f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#3f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#4f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#5f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#7f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              ),
-              $("#8f").animate(
-                {
-                  color: $.Color("#ffffff"),
-                },
-                {
-                  duration: 200,
-                  easing: "easeInOutQuad",
-                }
-              );
-          }
-        );
-      });
-      $(document).ready(function () {
         $("#7f").hover(
           function () {
             $("#1f").animate(
@@ -1458,8 +1169,6 @@
               );
           }
         );
-      });
-      $(document).ready(function () {
         $("#8f").hover(
           function () {
             $("#1f").animate(
